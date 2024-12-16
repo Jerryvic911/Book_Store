7a9daee99b38af35c906901fa6fe015364732032
jeremiahv <jeremiahv791@gmail.com>
2024-12-16 19:38:17 +0100

    https://github.com/Jerryvic911/Book_Store

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 3000;
-
-export const mongoDBURL =
-"mongodb+srv://jeremiahv791:wspzGIhGmudGd3bh@book-store.r5dkh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store";
